b94f876396bd0b901955b85c96d63e6d342d4a12
Areej-Manzoor <areejmanzoor456@gmail.com>
2021-08-24 17:00:41 -0500

    structures

---
+++ structure/(1) structure.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+
+using namespace std;
+
+struct Student
+{
+ string name;
+ int marks,rollno;
+};
+
+int main()
+{
+    Student s;
+  
+  cout<<"\t\t\t Student Entery"<<endl<<endl;
+  
+  cout<<"Enter Student Name";
+  cin>>s.name;
+  cout<<"Enter Student Marks";
+  cin>>s.marks;
+  cout<<"Enter Student Roll No";
+  cin>>s.rollno;
+  
+  cout<<"\t\t\t Student Entery"<<endl<<endl;
+  cout<<"Student Name : "<<s.name<<endl<<"Student Marks : "<<s.marks<<endl<<"Student Roll No : "<<s.rollno;
+    
+    
+  
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+
+
+
---
+++ structure/(2) initialize_structure (program 8.4).cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+using namespace std;
+
+// Structure Example :Program 8.4 from it series c++ book | page:251
+
+struct Employee
+{
+int eid;
+int sal;
+};
+
+int main()
+{
+   Employee e={1,4500}; // this way the variables in structure can be assigned some value
+    
+  cout<<"Intializing structure variables"<<endl<<endl;
+  
+  cout<<" Employee Id : "<<e.eid<<endl;
+  cout<<" Employee Salary : "<<e.sal<<endl;
+ 
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+
+
+
