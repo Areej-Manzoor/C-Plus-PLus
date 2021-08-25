2ed434c13225a30cfef9bd46a6a4b1194e3042de
Areej-Manzoor <areejmanzoor456@gmail.com>
2021-08-25 10:55:10 -0500

    enumaerations

---
+++ structure/(8) enumerationms.cpp (program 8.14).cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+using namespace std;
+
+// Enumerations Example :Program 8.14 from it series c++ book | page:263
+
+// Enumerations are used to create new data types,contains a list of values,Each value has unique number starts from 0.
+// Enumeration is a collection of named constants. Similar to integral numbers. But represented with names.
+
+enum year
+{january,february,march,april,may,june,july,agust,september,october,november,december};
+
+int main()
+{
+year y;
+  
+y=march;
+cout<<" Value of y :"<<y;
+  
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+
+
+
