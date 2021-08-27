6c93d670d405abf13413c05a4700c06cb39e8276
Areej-Manzoor <areejmanzoor456@gmail.com>
2021-08-27 11:13:58 -0500

    program 6

--- structure/practice programs/program 6.cpp
+++ structure/practice programs/program 6.cpp
@@ -14,11 +14,85 @@ struct Student
     char name[50];
 };
 
+void point1(Student s[11])
+{
+int age_counter=0;
+   cout<<endl;
+   cout<<"------------------------------------------"<<endl;
+   cout<<"Details of students between having age 14 "<<endl;
+   cout<<"------------------------------------------"<<endl;
+     for(int i=0;i<11;i++)
+    {
+      if(s[i].age==14)
+      {
+          cout<<"Student : "<<i+1<<endl;
+          cout<<"Student Name : "<<s[i].name<<endl;
+          cout<<"Student Roll No : "<<s[i].rno<<endl;
+          cout<<"Subject Age : "<<s[i].age<<endl;
+          age_counter++;
+      }
+     
+        
+    }
+     if(age_counter==0)
+      {
+          cout<<"No student found whose roll no is 14";
+      }
 
+}    
+
+void point2(Student s[11])
+{ 
+    int rno_counter=0;
+    cout<<endl;
+    cout<<"-----------------------------------------"<<endl;
+    cout<<"Details of students having even roll no's "<<endl;
+    cout<<"-----------------------------------------"<<endl;
+     for(int i=0;i<11;i++)
+    {
+      if((s[i].rno%2)==0)
+      {
+          cout<<"Student : "<<i+1<<endl;
+          cout<<"Student Name : "<<s[i].name<<endl;
+          cout<<"Student Roll No : "<<s[i].rno<<endl;
+          cout<<"Subject Age : "<<s[i].age<<endl;
+          rno_counter++;
+      }
+    
+        
+    }
+     if(rno_counter==0)
+      {
+          cout<<"No student found having even roll no";
+      }
+    
+} 
+
+void point3(Student s[11],int find)
+{   
+    cout<<endl;
+    cout<<"-------------------------------------------------------------------------------"<<endl;
+    cout<<"Details of students whose roll no is given (i.e. roll no. entered by the user) "<<endl;
+    cout<<"-------------------------------------------------------------------------------"<<endl;
+    for(int i=0;i<11;i++)
+    {
+        if(s[i].rno==find)
+        {
+           cout<<"Student : "<<i+1<<endl;
+          cout<<"Student Name : "<<s[i].name<<endl;
+          cout<<"Student Roll No : "<<s[i].rno<<endl;
+          cout<<"Subject Age : "<<s[i].age<<endl;   
+        }    
+  
+    } 
+}
 int main()
 {
     Student s[11]; // here 11 records can be entered which are more than 10
-    int age_counter=0,rno_counter=0;
+   cout<<"----------------------"<<endl;
+   cout<<"Enter Students Details "<<endl;
+   cout<<"----------------------"<<endl;
+    
     for(int i=0;i<11;i++)
     {
         cout<<"Student "<<i+1<<endl;
@@ -43,64 +117,18 @@ int main()
     cout<<endl<<endl;
     
     // 1 - Write a function to print the names of all the students having age 14.
-    
-    cout<<"Details of students between having age 14 "<<endl<<endl;
-     for(int i=0;i<11;i++)
-    {
-      if(s[i].age==14)
-      {
-          cout<<"Student : "<<i+1<<endl;
-          cout<<"Student Name : "<<s[i].name<<endl;
-          cout<<"Student Roll No : "<<s[i].rno<<endl;
-          cout<<"Subject Age : "<<s[i].age<<endl;
-          age_counter++;
-      }
-     
-        
-    }
-     if(age_counter==0)
-      {
-          cout<<"No student found whose roll no is 14";
-      }
+     point1(s);
     
     // 2 - Write another function to print the names of all the students having even roll no.
-    
-     cout<<endl<<"Details of students having even roll no's "<<endl<<endl;
-     for(int i=0;i<11;i++)
-    {
-      if((s[i].rno%2)==0)
-      {
-          cout<<"Student : "<<i+1<<endl;
-          cout<<"Student Name : "<<s[i].name<<endl;
-          cout<<"Student Roll No : "<<s[i].rno<<endl;
-          cout<<"Subject Age : "<<s[i].age<<endl;
-          rno_counter++;
-      }
-    
-        
-    }
-     if(rno_counter==0)
-      {
-          cout<<"No student found having even roll no";
-      }
+     point2(s);
     
     // 3 - Write another function to display the details of the student whose roll no is given (i.e. roll no. entered by the user).
-    
-      cout<<"Details of students whose roll no is given (i.e. roll no. entered by the user) "<<endl<<endl;
       int find;
-      cout<<"Enter a rno :";
+      cout<<endl<<endl;
+      cout<<"Enter a roll no to find :";
       cin>>find;
-     for(int i=0;i<11;i++)
-    {
-        if(s[i].rno==find)
-        {
-           cout<<"Student : "<<i+1<<endl;
-          cout<<"Student Name : "<<s[i].name<<endl;
-          cout<<"Student Roll No : "<<s[i].rno<<endl;
-          cout<<"Subject Age : "<<s[i].age<<endl;   
-        }    
-  
-    }
+      point3(s,find);
+    
   
     
 }
