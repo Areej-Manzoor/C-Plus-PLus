b7faf837bffe499badac53a3712c3b3a46b7e3e9
Areej-Manzoor <areejmanzoor456@gmail.com>
2021-08-28 15:49:04 -0500

    pointer and string basic concepts

--- pointers/(5) pointer_and_strings.cpp
+++ pointers/(5) pointer_and_strings.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    //A string is an array of characters.A pointer of type char can refer to the string. we do not need to assign ptr=&name like this as string is an array and array address is same as the name of the array variable so referring would be like  ptr=name;
+    
+//A string is an array of characters.A pointer of type char can refer to the string. we do not need to assign ptr=&name like this as string is an array and array address is same as the name of the array variable so referring would be like  ptr=name;
     
     // Both the pointer and string must be of same data type other wise it'll throw a type conversion error like this 
     // error: cannot convert ‘char [20]’ to ‘int*’ in assignment
     
 // The pointer displays the value of each element in array until a null character \0 is found.
-    //https://stackoverflow.com/questions/7949761/c-error-void-is-not-a-pointer-to-object-type
+    //https://stackoverflow.com/questions/7949761/c-error-void-is-not-a-pointer-to-object-type`
     
     
    cout <<"Arrays & Strings "<< endl<<endl; 
    
-   char name[20],*ptr,ch,found; 
+   char name[20],*ptr,ch,found;
+   found='n';
 
    ptr=name;
    cout<<"Enter a name = ";
@@ -25,13 +26,17 @@ int main()
   cout<<endl<<"Enter a character to find in above entered name =";
   cin>>ch;
  
-  while(*ptr !='\0')
-   {
+  while(*ptr!='\0')
+   { 
+     
      if(*ptr==ch)
-    found='y';
+   {
+        found='y';
+        
+   }
     *ptr++;
    }
-    
+ 
   if(found=='y')
   cout<<endl<<"Yes the character has been found";
   else
