11b5aca91103b51b15eba9c0ff83a3d4f7c34405
Areej-Manzoor <areejmanzoor456@gmail.com>
2021-08-28 13:23:06 -0500

    find string length using pointers practice program 9

---
+++ pointers/practice programs/program 9.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+// PROBLEM : Write a program in C to Calculate the length of the string using a pointer.
+
+int main()
+{
+   
+   char str[20],*ptr;
+   int counter;
+
+   ptr=str;
+   cout<<"Enter a string = ";
+   cin.get(str,20);
+
+ 
+  while(*ptr !='\0')
+   {
+   counter++; 
+   *ptr++;
+   }
+   
+   cout<<"Length of string '"<<str<<"' = "<<counter;
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+
+
+
