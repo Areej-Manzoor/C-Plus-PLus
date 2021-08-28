e670caaca8329fea4f2bbf4df3ec3107b3e49544
Areej-Manzoor <areejmanzoor456@gmail.com>
2021-08-28 11:20:00 -0500

    store n elem. in array & print using pointers practice program 6

---
+++ pointers/practice programs/program 6.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+// PROBLEM : Write a program in C++ to store n elements in an array and print the elements using pointer
+#define MAX_SIZE 100 //constant
+int main()
+{
+ int arr[MAX_SIZE],*ptr,n;
+ 
+ cout<<"Enter number of elements to store : ";
+ cin>>n;
+ ptr=arr;
+ for(int i=0;i<n;i++)
+ {
+  cout<<"Enter firsl element : ";
+  cin>>ptr[i];
+ }     
+ 
+ cout<<"Elements in array are as follows: "<<endl;
+ 
+ for(int i=0;i<n;i++)
+ {
+ cout<<ptr[i]<<endl; 
+ }  
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+
+
+
