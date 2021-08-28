1d0f6fc86de826f78707812d9f3f4e7ccbed4b5f
Areej-Manzoor <areejmanzoor456@gmail.com>
2021-08-28 10:43:43 -0500

    addition through pointers practice program 3

---
+++ pointers/practice programs/program 3.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include<cstring>
+using namespace std;
+
+// PROBLEM : Write a program in C++ to add two numbers using pointers
+
+// METHOD 1
+
+
+int main()
+{
+ int num1=5,num2=6,*ptr1,*ptr2;
+ 
+ ptr1=&num1;
+ ptr2=&num2;
+ 
+ cout<<"Sum of "<<*ptr1<<" & "<<*ptr2<<" = "<<*ptr1+*ptr2;
+ 
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+
+
+
