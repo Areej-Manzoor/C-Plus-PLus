1859cf5d8549edd2ec68c95f0266645e82d34af3
Areej-Manzoor <areejmanzoor456@gmail.com>
2021-08-31 11:20:03 -0500

    pointer to structure practice program 10

---
+++ structure/practice programs/program 10.cpp
@@ -0,0 +1,190 @@
+#include <iostream>
+
+using namespace std;
+
+// PROGRAM : Write a program in C++ to show the usage of pointer to structure
+
+struct Book
+{
+ int BookID,pages;
+ float price;
+};
+
+int main()
+{
+   Book b[2];
+   Book *ptr;
+  ptr=b;
+  for(int i=0;i<2;i++)
+  {
+  cout<<"Enter Book "<<i+1<<" Id :";
+  cin>>b[i].BookID;
+  cout<<"Enter Book "<<i+1<<" Price :";
+  cin>>b[i].price;
+  cout<<"Enter Book "<<i+1<<" Pages :";
+  cin>>b[i].pages;  
+  }
+  
+
+  for(int i=0;i<2;i++)
+  {
+        
+
+cout<<"Book "<<i+1<<" Id : "<<ptr->BookID<<endl;
+cout<<"Book "<<i+1<<" Price : "<<ptr->price<<endl;
+cout<<"Book "<<i+1<<" Pages : "<<ptr->pages<<endl;
+
+ptr++;
+    
+  }  
+
+
+    
+  
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+
+
+
