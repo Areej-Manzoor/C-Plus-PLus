766877d2566aef2f23abc388f20760955cc702f7
Areej-Manzoor <areejmanzoor456@gmail.com>
2021-09-02 11:33:58 -0500

    global variable & function practice  program 9.16

---
+++ functions/(8) global_variable program 9.16.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+// GLOBAL VARIABLES
+// Functions Example :Program 9.16 from it series c++ book | page:287
+
+int n;
+void mul()
+{
+    n=n*2;
+}
+int main()
+{
+ 
+  
+  cout<<"Enter a number : ";
+  cin>>n;
+  
+  cout<<"Value of n before multiplication :"<<n<<endl;
+  mul();
+  cout<<"Value of n after multiplying by 2 :"<<n;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+
+
+
